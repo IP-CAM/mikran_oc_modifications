abd1ea6328a7d5147dba6e101b114e75719e57ef
zdanozdan <tomasz.zdanowski@mikran.com>
2019-05-23 15:11:48 +0000

    added pl translations for cart

---
+++ catalog/language/pl-PL/checkout/cart.php
@@ -0,0 +1,5 @@
+<?php
+$_['column_tax_name']          = 'Podatek';
+$_['column_tax_rate']          = 'Stawka';
+$_['column_total_with_tax']    = 'Brutto';
+$_['column_mikran_id']         = 'Kod Mikran';
