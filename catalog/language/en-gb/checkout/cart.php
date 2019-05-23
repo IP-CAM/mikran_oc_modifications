6e0df834355154678682fca2c427bc2e91ac4a34
zdanozdan <tomasz.zdanowski@mikran.com>
2019-05-23 13:30:41 +0000

    added translation for extra mikran fields

---
+++ catalog/language/en-gb/checkout/cart.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['column_tax_name']          = 'Tax';
+$_['column_tax_rate']          = 'Tax rate';
+$_['column_total_with_tax']    = 'Total with tax';
+$_['column_mikran_id']         = 'Mikran ID';
