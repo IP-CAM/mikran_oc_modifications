4dd9dd48898121166f52d2bcd9dbd5e86a08de81
zdanozdan <tomasz.zdanowski@mikran.com>
2019-06-13 18:41:34 +0000

    Translation work in progress

--- admin/model/catalog/option.php
+++ admin/model/catalog/option.php
@@ -1,15 +1,7 @@
 <?php
 use Google\Cloud\Translate\TranslateClient;
 
-trait Filter {
-    function filterString() {
-        return "Filter by string";
-    }
-}
-
 class mikran_ModelCatalogOption extends ModelCatalogOption {
-
-    use Filter;
 
     public function getProductOptionValues($option_value_id) {
         $sql = "SELECT pd.product_id,pd.name FROM " . DB_PREFIX . "product_option_value pov left join ". DB_PREFIX ."product_description pd on (pov.product_id = pd.product_id)  WHERE pd.language_id = '". (int)$this->config->get('config_language_id') ."' and pov.option_value_id = '" . (int)$option_value_id . "'";
